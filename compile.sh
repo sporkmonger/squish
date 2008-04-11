94873908ff09af33999189ed9da25c82e2b5d852
Bob Aman <bob@sporkmonger.com>
2008-04-10 20:12:29 -0400

    Switched to using OMake for building.

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ocamlc -c bit_reader.ml
-ocamlc -c state.ml
-ocamlc -c traverse.ml
-ocamlc -c squish.ml
-ocamlc -c command_line.ml
-ocamlc -o squish bit_reader.cmo state.cmo traverse.cmo squish.cmo command_line.cmo
